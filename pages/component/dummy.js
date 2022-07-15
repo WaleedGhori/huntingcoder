d62ce98863707ade44d11254e1556456af700b24
WaleedGhori <96137855+WaleedGhori@users.noreply.github.com>
2022-07-15 13:12:49 +0500

    Initial commit

---
+++ pages/component/dummy.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Dummy = () => {
+  return (
+    <>
+    <style jsx>
+        {`
+        .dummy{
+            background:red
+        }
+        `}
+    </style>
+    <div className='dummy'>This page is a Dummy class</div>
+    </>
+  )
+}
+
+export default Dummy
